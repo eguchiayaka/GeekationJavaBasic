dd3eaace4f2f99be4f03a2b35545c87c45809dab
eguchiayaka <eguchiayaka.524@gmail.com>
2024-05-02 19:45:47 +0900

    変数の実装

---
+++ src/basicSyntax/Variables.java
@@ -0,0 +1,20 @@
+package basicSyntax;
+
+public class Variables {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+       int num1 = 5;
+       double num2 = 24.2;
+       String first1 = "私の好きな整数は";
+       String first2 = "私の好きな少数は";
+       String last = "です";
+       
+       System.out.println(num1);
+       System.out.println(num2);
+       System.out.println(first1 + num1 + last);
+       System.out.println(first2 + num2 + last);
+       
+	}
+
+}
